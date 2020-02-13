8d4e31fe09512a121768f1b0d42d9ef1f62bab4d
Jeff Irion <jefflirion@users.noreply.github.com>
2020-02-12 20:50:43 -0800

    Documentation fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -112,7 +112,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'PythonPackagedoc'
+htmlhelp_basename = 'GraphSLAMdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -139,7 +139,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'PythonPackage.tex', 'Python Package Documentation',
+    (master_doc, 'graphslam.tex', 'graphslam Documentation',
      'Jeff Irion', 'manual'),
 ]
 
@@ -149,7 +149,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'graphslam', 'Python Package Documentation',
+    (master_doc, 'graphslam', 'graphslam Documentation',
      [author], 1)
 ]
 
@@ -160,8 +160,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'PythonPackage', 'Python Package Documentation',
-     author, 'PythonPackage', 'One line description of project.',
+    (master_doc, 'graphslam', 'graphslam Documentation',
+     author, 'graphslam', 'Graph SLAM solver in Pythonn',
      'Miscellaneous'),
 ]
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,10 +1,10 @@
-.. Jeff Irion's Python package documentation master file, created by
+.. Jeff Irion's `graphslam` documentation master file, created by
    sphinx-quickstart on Mon Sep 05 22:06:10 2016.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-python\_package Documentation
-=============================
+graphslam Documentation
+=======================
 
 .. toctree::
    :hidden:
