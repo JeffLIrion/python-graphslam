Implement BasePose.__iadd__, fix SE(2) boxplus Jacobian

graphslam/pose/se2.py
@@ -251,4 +251,6 @@ class PoseSE2(BasePose):
             The Jacobian of :math:`p_1 \boxplus \Delta \mathbf{x}` w.r.t. :math:`\Delta \mathbf{x}` evaluated at :math:`\Delta \mathbf{x} = \mathbf{0}`
 
         """
-        return np.eye(3)
+        return np.array([[np.cos(self[2]), -np.sin(self[2]), 0.],
+                         [np.sin(self[2]), np.cos(self[2]), 0.],
+                         [0., 0., 1.]])

tests/test_pose_se3.py
@@ -149,8 +149,10 @@ class TestPoseSE3(unittest.TestCase):
             p1.normalize()
 
             expected = np.dot(p1.to_matrix(), p2.to_matrix())
-            self.assertAlmostEqual(np.linalg.norm((p1 + p2_compact).to_matrix()[:, 3] - expected[:, 3]), 0.)
-            self.assertAlmostEqual(np.linalg.norm((p1 + p2_compact).to_matrix()[:, :3] - expected[:, :3]), 0.)
+            self.assertAlmostEqual(np.linalg.norm((p1 + p2_compact).to_matrix() - expected), 0.)
+
+            p1 += p2_compact
+            self.assertAlmostEqual(np.linalg.norm(p1.to_matrix() - expected), 0.)
 
         with self.assertRaises(NotImplementedError):
             p1 = PoseSE3(np.random.random_sample(3), np.random.random_sample(4))