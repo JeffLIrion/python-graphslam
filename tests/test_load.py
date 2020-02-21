33f01a8d5002b84982193d231eeb1571af547149
Jeff Irion <JeffLIrion@users.noreply.github.com>
2020-02-20 19:38:16 -0800

    Add support for loading SE(3) datasets from .g2o files (#4)
    * Implement and test 'load_g2o_se3'
    
    * Remove print statements

--- tests/test_load.py
+++ tests/test_load.py
@@ -8,7 +8,7 @@
 import os
 import unittest
 
-from graphslam.load import load_g2o_se2
+from graphslam.load import load_g2o_se2, load_g2o_se3
 
 
 class TestLoad(unittest.TestCase):
@@ -21,5 +21,14 @@ class TestLoad(unittest.TestCase):
         """
         infile = os.path.join(os.path.dirname(__file__), 'test_se2.g2o')
         g = load_g2o_se2(infile)
+
+        self.assertGreater(g.calc_chi2(), 0.)
+
+    def test_load_g2o_se3(self):
+        """Test the ``load_g2o_se3()`` function.
+
+        """
+        infile = os.path.join(os.path.dirname(__file__), 'test_se3.g2o')
+        g = load_g2o_se3(infile)
 
         self.assertGreater(g.calc_chi2(), 0.)
