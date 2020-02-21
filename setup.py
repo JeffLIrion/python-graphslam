a54cfeec9e1ea30879eeb79e473670efeb0155c0
Jeff Irion <jefflirion@users.noreply.github.com>
2020-02-20 19:40:34 -0800

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as f:
 
 setup(
     name='graphslam',
-    version='0.0.1',
+    version='0.0.2',
     description='Graph SLAM solver in Python',
     long_description=readme,
     keywords=['graphslam', 'slam', 'graph', 'optimization', 'mapping'],
