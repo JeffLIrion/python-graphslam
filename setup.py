9ef40808d9c14595e94d75782a3d2f23df1a7a4f
Jeff Irion <JeffLIrion@users.noreply.github.com>
2023-10-19 19:59:09 -0700

    Update scripts (#34)
    * Apply black formatting to setup.py and __init__.py
    
    * Update scripts
    
    * Add version in __init__.py
    
    * Update git_tag.sh

--- setup.py
+++ setup.py
@@ -7,24 +7,34 @@
 
 from setuptools import setup
 
-with open('README.rst') as f:
-    readme = f.read().replace(':math:', '').replace('\\mathbb{R}', 'R').replace('.. image::                                                                                  ', '.. image:: https://raw.githubusercontent.com/JeffLIrion/python-graphslam/master/docs/source/')
+with open("README.rst") as f:
+    readme = (
+        f.read()
+        .replace(":math:", "")
+        .replace("\\mathbb{R}", "R")
+        .replace(
+            ".. image::                                                                                  ",
+            ".. image:: https://raw.githubusercontent.com/JeffLIrion/python-graphslam/master/docs/source/",
+        )
+    )
 
 setup(
-    name='graphslam',
-    version='0.0.8',
-    description='Graph SLAM solver in Python',
+    name="graphslam",
+    version="0.0.8",
+    description="Graph SLAM solver in Python",
     long_description=readme,
-    keywords=['graphslam', 'slam', 'graph', 'optimization', 'mapping'],
-    url='https://github.com/JeffLIrion/python-graphslam',
-    license='MIT',
-    author='Jeff Irion',
-    author_email='jefflirion@users.noreply.github.com',
-    packages=['graphslam', 'graphslam.pose', 'graphslam.edge'],
-    install_requires=['numpy', 'scipy'],
-    tests_require=['matplotlib'],
-    classifiers=['License :: OSI Approved :: MIT License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 3'],
-    test_suite='tests'
+    keywords=["graphslam", "slam", "graph", "optimization", "mapping"],
+    url="https://github.com/JeffLIrion/python-graphslam",
+    license="MIT",
+    author="Jeff Irion",
+    author_email="jefflirion@users.noreply.github.com",
+    packages=["graphslam", "graphslam.pose", "graphslam.edge"],
+    install_requires=["numpy", "scipy"],
+    tests_require=["matplotlib"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    test_suite="tests",
 )
