dee98e76be8c0832f5a6683bbe0dd9e612080661
Jeff Irion <jefflirion@users.noreply.github.com>
2023-11-03 22:46:14 -0700

    WIP

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -22,14 +22,10 @@ from graphslam.vertex import Vertex
 
 
 class TestGraphR2(unittest.TestCase):
-    r"""Tests for the ``Graph`` class with :math:`\mathbb{R}^2` poses.
-
-    """
+    r"""Tests for the ``Graph`` class with :math:`\mathbb{R}^2` poses."""
 
     def setUp(self):
-        r"""Setup a simple ``Graph`` in :math:`\mathbb{R}^2`.
-
-        """
+        r"""Setup a simple ``Graph`` in :math:`\mathbb{R}^2`."""
         np.random.seed(0)
 
         p1 = PoseR2(np.random.random_sample(2))
@@ -47,17 +43,13 @@ class TestGraphR2(unittest.TestCase):
         self.g = Graph([e1, e2], [v1, v2, v3])
 
     def test_calc_chi2(self):
-        r"""Test that the :math:`\chi^2` for a ``Graph`` can be computed.
-
-        """
+        r"""Test that the :math:`\chi^2` for a ``Graph`` can be computed."""
         chi2 = self.g._edges[0].calc_chi2() + self.g._edges[1].calc_chi2()  # pylint: disable=protected-access
 
         self.assertAlmostEqual(chi2, self.g.calc_chi2())
 
     def test_optimize(self):
-        """Test that a ``Graph`` can be optimized.
-
-        """
+        """Test that a ``Graph`` can be optimized."""
         chi2_orig = self.g.calc_chi2()
 
         p0 = self.g._vertices[0].pose.to_array()  # pylint: disable=protected-access
@@ -65,12 +57,12 @@ class TestGraphR2(unittest.TestCase):
         self.assertLess(self.g.calc_chi2(), chi2_orig)
 
         # Make sure the first pose was held fixed
+        # fmt: off
         self.assertAlmostEqual(np.linalg.norm(p0 - self.g._vertices[0].pose.to_array()), 0.)  # pylint: disable=protected-access
+        # fmt: on
 
     def _test_optimize_fixed_vertices(self, fixed_indices):
-        """Test that a ``Graph`` can be optimized with vertices held fixed.
-
-        """
+        """Test that a ``Graph`` can be optimized with vertices held fixed."""
         chi2_orig = self.g.calc_chi2()
 
         poses_before = [self.g._vertices[i].pose.to_array() for i in fixed_indices]  # pylint: disable=protected-access
@@ -83,43 +75,31 @@ class TestGraphR2(unittest.TestCase):
         # Make sure the poses were held fixed
         poses_after = [self.g._vertices[i].pose.to_array() for i in fixed_indices]  # pylint: disable=protected-access
         for before, after in zip(poses_before, poses_after):
-            self.assertAlmostEqual(np.linalg.norm(before - after), 0.)
+            self.assertAlmostEqual(np.linalg.norm(before - after), 0.0)
 
     def test_optimize_fix_1(self):
-        """Test that the ``optimize`` method works correctly when fixing vertex 1.
-
-        """
+        """Test that the ``optimize`` method works correctly when fixing vertex 1."""
         self._test_optimize_fixed_vertices([1])
 
     def test_optimize_fix_2(self):
-        """Test that the ``optimize`` method works correctly when fixing vertex 2.
-
-        """
+        """Test that the ``optimize`` method works correctly when fixing vertex 2."""
         self._test_optimize_fixed_vertices([2])
 
     def test_optimize_fix_01(self):
-        """Test that the ``optimize`` method works correctly when fixing vertices 0 and 1.
-
-        """
+        """Test that the ``optimize`` method works correctly when fixing vertices 0 and 1."""
         self._test_optimize_fixed_vertices([0, 1])
 
     def test_optimize_fix_02(self):
-        """Test that the ``optimize`` method works correctly when fixing vertices 0 and 2.
-
-        """
+        """Test that the ``optimize`` method works correctly when fixing vertices 0 and 2."""
         self._test_optimize_fixed_vertices([0, 2])
 
     def test_optimize_fix_12(self):
-        """Test that the ``optimize`` method works correctly when fixing vertices 1 and 2.
-
-        """
+        """Test that the ``optimize`` method works correctly when fixing vertices 1 and 2."""
         self._test_optimize_fixed_vertices([1, 2])
 
     # pylint: disable=protected-access
     def test_to_g2o(self):
-        """Test that the ``to_g2o`` method is implemented correctly, or raises ``NotImplementedError``.
-
-        """
+        """Test that the ``to_g2o`` method is implemented correctly, or raises ``NotImplementedError``."""
         # Supported types
         if isinstance(self.g._vertices[0].pose, (PoseSE2, PoseSE3)):
             print(self.g._vertices[0].to_g2o())
@@ -141,9 +121,7 @@ class TestGraphR2(unittest.TestCase):
                     self.g.to_g2o("test.g2o")
 
     def test_plot(self):
-        """Test that the ``plot`` method does not raise an exception.
-
-        """
+        """Test that the ``plot`` method does not raise an exception."""
         # avoid showing the plots
         with patch("graphslam.graph.plt.show"):
             self.g.plot(title="Title")
@@ -155,14 +133,10 @@ class TestGraphR2(unittest.TestCase):
 
 
 class TestGraphR3(TestGraphR2):
-    r"""Tests for the ``Graph`` class with :math:`\mathbb{R}^3` poses.
-
-    """
+    r"""Tests for the ``Graph`` class with :math:`\mathbb{R}^3` poses."""
 
     def setUp(self):
-        r"""Setup a simple ``Graph`` in :math:`\mathbb{R}^3`.
-
-        """
+        r"""Setup a simple ``Graph`` in :math:`\mathbb{R}^3`."""
         np.random.seed(0)
 
         p1 = PoseR3(np.random.random_sample(3))
@@ -181,20 +155,16 @@ class TestGraphR3(TestGraphR2):
 
 
 class TestGraphSE2(TestGraphR2):
-    r"""Tests for the ``Graph`` class with :math:`SE(2)` poses.
-
-    """
+    r"""Tests for the ``Graph`` class with :math:`SE(2)` poses."""
 
     def setUp(self):
-        r"""Setup a simple ``Graph`` in :math:`SE(2)`.
-
-        """
+        r"""Setup a simple ``Graph`` in :math:`SE(2)`."""
         np.random.seed(0)
 
         p1 = PoseSE2(np.random.random_sample(2), np.random.random_sample())
         p2 = PoseSE2(np.random.random_sample(2), np.random.random_sample())
         p3 = PoseSE2(np.random.random_sample(2), np.random.random_sample())
-        estimate = PoseSE2([0, 0], 0.)
+        estimate = PoseSE2([0, 0], 0.0)
 
         v1 = Vertex(1, p1)
         v2 = Vertex(2, p2)
@@ -207,14 +177,10 @@ class TestGraphSE2(TestGraphR2):
 
 
 class TestGraphSE3(TestGraphR2):
-    r"""Tests for the ``Graph`` class with :math:`SE(3)` poses.
-
-    """
+    r"""Tests for the ``Graph`` class with :math:`SE(3)` poses."""
 
     def setUp(self):
-        r"""Setup a simple ``Graph`` in :math:`SE(3)`.
-
-        """
+        r"""Setup a simple ``Graph`` in :math:`SE(3)`."""
         np.random.seed(0)
 
         p1 = PoseSE3(np.random.random_sample(3), np.random.random_sample(4))
@@ -260,7 +226,7 @@ class TestGraphR2SE2(unittest.TestCase):
         p4 = PoseSE2(np.random.random_sample(2), np.random.random_sample())
         p5 = PoseSE2(np.random.random_sample(2), np.random.random_sample())
         p6 = PoseSE2(np.random.random_sample(2), np.random.random_sample())
-        estimate_se2 = PoseSE2([0, 0], 0.)
+        estimate_se2 = PoseSE2([0, 0], 0.0)
 
         v4 = Vertex(4, p4)
         v5 = Vertex(5, p5)
@@ -355,5 +321,5 @@ class TestGraphOptimization(unittest.TestCase):
         self.assertTrue(g.approx_equal(g2))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- tests/test_pose_r3.py
+++ tests/test_pose_r3.py
@@ -16,23 +16,17 @@ from .edge_oplus_ominus import EdgeOMinus, EdgeOMinusCompact, EdgeOPlus, EdgeOPl
 
 
 class TestPoseR3(unittest.TestCase):
-    """Tests for the ``PoseR3`` class.
-
-    """
+    """Tests for the ``PoseR3`` class."""
 
     def test_constructor(self):
-        """Test that a ``PoseR3`` instance can be created.
-
-        """
+        """Test that a ``PoseR3`` instance can be created."""
         r3a = PoseR3([1, 2, 3])
         r3b = PoseR3(np.array([3, 4, 5]))
         self.assertIsInstance(r3a, PoseR3)
         self.assertIsInstance(r3b, PoseR3)
 
     def test_copy(self):
-        """Test that the ``copy`` method works as expected.
-
-        """
+        """Test that the ``copy`` method works as expected."""
         p1 = PoseR3([1, 2, 3])
         p2 = p1.copy()
 
@@ -40,26 +34,22 @@ class TestPoseR3(unittest.TestCase):
         self.assertEqual(p1[0], 1)
 
     def test_to_array(self):
-        """Test that the ``to_array`` method works as expected.
-
-        """
+        """Test that the ``to_array`` method works as expected."""
         r3 = PoseR3([1, 2, 3])
         arr = r3.to_array()
 
         self.assertIsInstance(arr, np.ndarray)
         self.assertNotIsInstance(arr, PoseR3)
-        self.assertAlmostEqual(np.linalg.norm(arr - np.array([1, 2, 3])), 0.)
+        self.assertAlmostEqual(np.linalg.norm(arr - np.array([1, 2, 3])), 0.0)
 
     def test_to_compact(self):
-        """Test that the ``to_compact`` method works as expected.
-
-        """
+        """Test that the ``to_compact`` method works as expected."""
         r3 = PoseR3([1, 2, 3])
         arr = r3.to_compact()
 
         self.assertIsInstance(arr, np.ndarray)
         self.assertNotIsInstance(arr, PoseR3)
-        self.assertAlmostEqual(np.linalg.norm(arr - np.array([1, 2, 3])), 0.)
+        self.assertAlmostEqual(np.linalg.norm(arr - np.array([1, 2, 3])), 0.0)
 
     # ======================================================================= #
     #                                                                         #
@@ -67,34 +57,28 @@ class TestPoseR3(unittest.TestCase):
     #                                                                         #
     # ======================================================================= #
     def test_position(self):
-        """Test that the ``position`` property works as expected.
-
-        """
+        """Test that the ``position`` property works as expected."""
         r3 = PoseR3([1, 2, 3])
         pos = r3.position
 
         true_pos = np.array([1, 2, 3])
         self.assertIsInstance(pos, np.ndarray)
         self.assertNotIsInstance(pos, PoseR3)
-        self.assertAlmostEqual(np.linalg.norm(true_pos - pos), 0.)
+        self.assertAlmostEqual(np.linalg.norm(true_pos - pos), 0.0)
 
     def test_orientation(self):
-        """Test that the ``orientation`` property works as expected.
-
-        """
+        """Test that the ``orientation`` property works as expected."""
         r3 = PoseR3([1, 2, 3])
 
-        self.assertEqual(r3.orientation, 0.)
+        self.assertEqual(r3.orientation, 0.0)
 
     def test_inverse(self):
-        """Test that the ``inverse`` property works as expected.
-
-        """
+        """Test that the ``inverse`` property works as expected."""
         r3 = PoseR3([1, 2, 3])
 
         true_inv = np.array([-1, -2, -3])
 
-        self.assertAlmostEqual(np.linalg.norm(r3.inverse.to_array() - true_inv), 0.)
+        self.assertAlmostEqual(np.linalg.norm(r3.inverse.to_array() - true_inv), 0.0)
 
     # ======================================================================= #
     #                                                                         #
@@ -102,24 +86,20 @@ class TestPoseR3(unittest.TestCase):
     #                                                                         #
     # ======================================================================= #
     def test_add(self):
-        """Test that the overloaded ``__add__`` method works as expected.
-
-        """
+        """Test that the overloaded ``__add__`` method works as expected."""
         r3a = PoseR3([1, 2, 3])
         r3b = PoseR3([3, 4, 5])
 
         expected = PoseR3([4, 6, 8])
-        self.assertAlmostEqual(np.linalg.norm(((r3a + r3b) - expected).to_array()), 0.)
+        self.assertAlmostEqual(np.linalg.norm(((r3a + r3b) - expected).to_array()), 0.0)
 
     def test_sub(self):
-        """Test that the overloaded ``__sub__`` method works as expected.
-
-        """
+        """Test that the overloaded ``__sub__`` method works as expected."""
         r3a = PoseR3([1, 2, 3])
         r3b = PoseR3([3, 4, 5])
 
         expected = PoseR3([2, 2, 2])
-        self.assertAlmostEqual(np.linalg.norm(((r3b - r3a) - expected).to_array()), 0.)
+        self.assertAlmostEqual(np.linalg.norm(((r3b - r3a) - expected).to_array()), 0.0)
 
     # ======================================================================= #
     #                                                                         #
@@ -127,9 +107,7 @@ class TestPoseR3(unittest.TestCase):
     #                                                                         #
     # ======================================================================= #
     def test_jacobian_self_oplus_other(self):
-        """Test that the ``jacobian_self_oplus_other_wrt_self`` and ``jacobian_self_oplus_other_wrt_other`` methods are correctly implemented.
-
-        """
+        """Test that the ``jacobian_self_oplus_other_wrt_self`` and ``jacobian_self_oplus_other_wrt_other`` methods are correctly implemented."""
         np.random.seed(0)
 
         for _ in range(10):
@@ -147,12 +125,10 @@ class TestPoseR3(unittest.TestCase):
 
             self.assertEqual(len(numerical_jacobians), len(analytical_jacobians))
             for n, a in zip(numerical_jacobians, analytical_jacobians):
-                self.assertAlmostEqual(np.linalg.norm(n - a), 0.)
+                self.assertAlmostEqual(np.linalg.norm(n - a), 0.0)
 
     def test_jacobian_self_ominus_other(self):
-        """Test that the ``jacobian_self_ominus_other_wrt_self`` and ``jacobian_self_ominus_other_wrt_other`` methods are correctly implemented.
-
-        """
+        """Test that the ``jacobian_self_ominus_other_wrt_self`` and ``jacobian_self_ominus_other_wrt_other`` methods are correctly implemented."""
         np.random.seed(0)
 
         for _ in range(10):
@@ -170,12 +146,10 @@ class TestPoseR3(unittest.TestCase):
 
             self.assertEqual(len(numerical_jacobians), len(analytical_jacobians))
             for n, a in zip(numerical_jacobians, analytical_jacobians):
-                self.assertAlmostEqual(np.linalg.norm(n - a), 0.)
+                self.assertAlmostEqual(np.linalg.norm(n - a), 0.0)
 
     def test_jacobian_self_oplus_other_compact(self):
-        """Test that the ``jacobian_self_oplus_other_wrt_self_compact`` and ``jacobian_self_oplus_other_wrt_other_compact`` methods are correctly implemented.
-
-        """
+        """Test that the ``jacobian_self_oplus_other_wrt_self_compact`` and ``jacobian_self_oplus_other_wrt_other_compact`` methods are correctly implemented."""
         np.random.seed(0)
 
         for _ in range(10):
@@ -193,12 +167,10 @@ class TestPoseR3(unittest.TestCase):
 
             self.assertEqual(len(numerical_jacobians), len(analytical_jacobians))
             for n, a in zip(numerical_jacobians, analytical_jacobians):
-                self.assertAlmostEqual(np.linalg.norm(n - a), 0.)
+                self.assertAlmostEqual(np.linalg.norm(n - a), 0.0)
 
     def test_jacobian_self_ominus_other_compact(self):
-        """Test that the ``jacobian_self_ominus_other_wrt_self_compact`` and ``jacobian_self_ominus_other_wrt_other_compact`` methods are correctly implemented.
-
-        """
+        """Test that the ``jacobian_self_ominus_other_wrt_self_compact`` and ``jacobian_self_ominus_other_wrt_other_compact`` methods are correctly implemented."""
         np.random.seed(0)
 
         for _ in range(10):
@@ -216,8 +188,8 @@ class TestPoseR3(unittest.TestCase):
 
             self.assertEqual(len(numerical_jacobians), len(analytical_jacobians))
             for n, a in zip(numerical_jacobians, analytical_jacobians):
-                self.assertAlmostEqual(np.linalg.norm(n - a), 0.)
+                self.assertAlmostEqual(np.linalg.norm(n - a), 0.0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
