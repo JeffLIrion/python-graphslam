Fix bug in SE(3) pose composition Jacobians

graphslam/pose/se3.py
@@ -250,7 +250,7 @@ class PoseSE3(BasePose):
         """
         # fmt: off
         return np.array([[1., 0., 0., 2. * self[4] * other[1] + 2. * self[5] * other[2], -4. * self[4] * other[0] + 2. * self[3] * other[1] + 2. * self[6] * other[2], -4. * self[5] * other[0] - 2. * self[6] * other[1] + 2. * self[3] * other[2], -2. * self[5] * other[1] + 2. * self[4] * other[2]],
-                         [0., 1., 0., 2. * self[4] * other[0] - 4. * self[3] * other[1] - 2. * self[6] * other[2], 2. * self[3] * other[0] + 2. * self[5] * other[2], -2. * self[6] * other[0] - 4. * self[5] * other[1] + self[4] * other[2], 2. * self[5] * other[0] - 2. * self[3] * other[2]],
+                         [0., 1., 0., 2. * self[4] * other[0] - 4. * self[3] * other[1] - 2. * self[6] * other[2], 2. * self[3] * other[0] + 2. * self[5] * other[2], -2. * self[6] * other[0] - 4. * self[5] * other[1] + 2. * self[4] * other[2], 2. * self[5] * other[0] - 2. * self[3] * other[2]],
                          [0., 0., 1., 2. * self[5] * other[0] + 2. * self[6] * other[1] - 4. * self[3] * other[2], -2. * self[6] * other[0] + 2. * self[5] * other[1] - 4. * self[4] * other[2], 2. * self[3] * other[0] + 2. * self[4] * other[1], -2. * self[4] * other[0] + 2. * self[3] * other[1]],
                          [0., 0., 0., other[6], other[5], -other[4], other[3]],
                          [0., 0., 0., -other[5], other[6], other[3], other[4]],
@@ -275,7 +275,7 @@ class PoseSE3(BasePose):
         """
         # fmt: off
         return np.array([[1., 0., 0., 2. * self[4] * other[1] + 2. * self[5] * other[2], -4. * self[4] * other[0] + 2. * self[3] * other[1] + 2. * self[6] * other[2], -4. * self[5] * other[0] - 2. * self[6] * other[1] + 2. * self[3] * other[2], -2. * self[5] * other[1] + 2. * self[4] * other[2]],
-                         [0., 1., 0., 2. * self[4] * other[0] - 4. * self[3] * other[1] - 2. * self[6] * other[2], 2. * self[3] * other[0] + 2. * self[5] * other[2], -2. * self[6] * other[0] - 4. * self[5] * other[1] + self[4] * other[2], 2. * self[5] * other[0] - 2. * self[3] * other[2]],
+                         [0., 1., 0., 2. * self[4] * other[0] - 4. * self[3] * other[1] - 2. * self[6] * other[2], 2. * self[3] * other[0] + 2. * self[5] * other[2], -2. * self[6] * other[0] - 4. * self[5] * other[1] + 2. * self[4] * other[2], 2. * self[5] * other[0] - 2. * self[3] * other[2]],
                          [0., 0., 1., 2. * self[5] * other[0] + 2. * self[6] * other[1] - 4. * self[3] * other[2], -2. * self[6] * other[0] + 2. * self[5] * other[1] - 4. * self[4] * other[2], 2. * self[3] * other[0] + 2. * self[4] * other[1], -2. * self[4] * other[0] + 2. * self[3] * other[1]],
                          [0., 0., 0., other[6], other[5], -other[4], other[3]],
                          [0., 0., 0., -other[5], other[6], other[3], other[4]],

tests/test_pose_se3.py
@@ -195,8 +195,8 @@ class TestPoseSE3(unittest.TestCase):
             analytical_jacobians = e.calc_jacobians()
 
             self.assertEqual(len(numerical_jacobians), len(analytical_jacobians))
-            for n, a in zip(numerical_jacobians, analytical_jacobians):
-                self.assertAlmostEqual(np.linalg.norm(n[:, :3] - a[:, :3]), 0.0)
+            for n, a in zip(numerical_jacobians[1:], analytical_jacobians[1:]):
+                self.assertAlmostEqual(np.linalg.norm(n - a), 0.0, places=5)
 
     def test_jacobian_self_ominus_other(self):
         """Test that the ``jacobian_self_ominus_other_wrt_self`` and ``jacobian_self_ominus_other_wrt_other`` methods are correctly implemented."""
@@ -243,8 +243,8 @@ class TestPoseSE3(unittest.TestCase):
             analytical_jacobians = e.calc_jacobians()
 
             self.assertEqual(len(numerical_jacobians), len(analytical_jacobians))
-            for n, a in zip(numerical_jacobians, analytical_jacobians):
-                self.assertAlmostEqual(np.linalg.norm(n[:, :3] - a[:, :3]), 0.0)
+            for n, a in zip(numerical_jacobians[1:], analytical_jacobians[1:]):
+                self.assertAlmostEqual(np.linalg.norm(n - a), 0.0, places=5)
 
     def test_jacobian_self_ominus_other_compact(self):
         """Test that the ``jacobian_self_ominus_other_wrt_self_compact`` and ``jacobian_self_ominus_other_wrt_other_compact`` methods are correctly implemented."""