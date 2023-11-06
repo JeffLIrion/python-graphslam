Fix bug in SE(3) pose composition Jacobians (#51)
* Fix bug in SE(3) pose composition Jacobians

* Another fix

tests/test_pose_se3.py
@@ -196,7 +196,7 @@ class TestPoseSE3(unittest.TestCase):
 
             self.assertEqual(len(numerical_jacobians), len(analytical_jacobians))
             for n, a in zip(numerical_jacobians, analytical_jacobians):
-                self.assertAlmostEqual(np.linalg.norm(n[:, :3] - a[:, :3]), 0.0)
+                self.assertAlmostEqual(np.linalg.norm(n - a), 0.0, places=5)
 
     def test_jacobian_self_ominus_other(self):
         """Test that the ``jacobian_self_ominus_other_wrt_self`` and ``jacobian_self_ominus_other_wrt_other`` methods are correctly implemented."""
@@ -244,7 +244,7 @@ class TestPoseSE3(unittest.TestCase):
 
             self.assertEqual(len(numerical_jacobians), len(analytical_jacobians))
             for n, a in zip(numerical_jacobians, analytical_jacobians):
-                self.assertAlmostEqual(np.linalg.norm(n[:, :3] - a[:, :3]), 0.0)
+                self.assertAlmostEqual(np.linalg.norm(n - a), 0.0, places=5)
 
     def test_jacobian_self_ominus_other_compact(self):
         """Test that the ``jacobian_self_ominus_other_wrt_self_compact`` and ``jacobian_self_ominus_other_wrt_other_compact`` methods are correctly implemented."""